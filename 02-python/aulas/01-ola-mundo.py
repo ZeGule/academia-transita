a559614cf1747163d350c025515677ef4fcdbbc2
ZeGule <143552102+ZeGule@users.noreply.github.com>
2026-07-11 20:49:11 -0300

    Criar aula 01 - Olá mundo

---
+++ 02-python/aulas/01-ola-mundo.py
@@ -0,0 +1,34 @@
+"""
+============================================================
+Academia Transita
+Módulo 02 - Python
+
+Aula 01 - Olá Mundo
+
+Objetivo:
+Conhecer a estrutura básica de um programa em Python,
+executar o primeiro código e compreender o funcionamento
+da função print().
+
+Autor: Erick
+============================================================
+"""
+
+
+print ("olá, Erick!")
+
+print("Academia Transita")
+print("Python")
+print("Meu primeiro projeto")
+print("Academia","Transita", "Python", "Meu primeiro projeto", sep=" / ")
+
+# ============================================================
+# Conclusões
+# ============================================================
+
+# 1. O Python executa o programa de cima para baixo.
+# 2. A função print() exibe informações na tela.
+# 3. Textos (strings) são escritos entre aspas.
+# 4. Uma função pode receber argumentos entre parênteses.
+# 5. O primeiro passo para programar é transformar uma ideia
+#    em instruções que o computador consiga executar.
