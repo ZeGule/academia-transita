cf3936b48619b094b24b4614b215c2ae8bdcdba1
ZeGule <143552102+ZeGule@users.noreply.github.com>
2026-07-11 20:50:01 -0300

    Cria a aula 02 - Variaveis

---
+++ 02-python/aulas/02-variaveis.py
@@ -0,0 +1,31 @@
+"""
+Academia Transita
+Módulo 02 - Python
+
+Aula 02 - Variáveis
+
+Objetivo:
+Aprender a criar, alterar e utilizar variáveis em Python.
+
+Autor: Erick
+Data: (não precisa preencher manualmente)
+"""
+
+nome = "Erick"
+print (nome)
+nome = "João"
+print (nome)
+
+idade = 29
+nova_idade = idade
+idade = 30
+print (nova_idade)
+print (idade)
+
+# -----------------------------------------
+# Conclusões
+# -----------------------------------------
+# Uma variável possui um nome e um valor.
+# O operador = faz uma atribuição, não uma comparação.
+# Ao fazer nova_idade = idade, o valor atual é atribuído à nova variável.
+# O Python executa o programa sequencialmente, mas cada atribuição acontece no momento em que a linha é executada.
