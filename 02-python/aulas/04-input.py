6abba4031c3bad9fa4f94a3c9e3c71f340f46a4e
ZeGule <143552102+ZeGule@users.noreply.github.com>
2026-07-12 20:02:43 -0300

    Adiciona aula 04 sobre entrada de dados

---
+++ 02-python/aulas/04-input.py
@@ -0,0 +1,8 @@
+nome = input("Qual é o seu nome? ")
+idade = input("Qual é a sua idade? ")
+profissao = input("Qual é a sua profissão? ")
+print("==============================")
+print("Olá", nome + "!")
+print("Você tem", idade, "anos")
+print("Sua profissão é", profissao)
+print("==============================")
